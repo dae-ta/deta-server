34e910951a4f203ad74b7d38357216c6e8d31fc5
kimheechan <kimhecan@rainist.com>
2024-01-22 21:02:44 +0900

    controllers service 분리하는 이유

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,9 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [],
+  // router
   controllers: [AppController],
+  // 비지니스로직의 분리 transaction 단위로 짜는게 좋음, 요청과 응답에 대해서는 관심없어야함. 테스트, 재사용 쉬워짐.
   providers: [AppService],
 })
 export class AppModule {}
